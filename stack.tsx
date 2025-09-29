9e3a976f96715fff4dab60adaaedda5bedff7345
Montinou <agusmontoya@gmail.com>
2025-09-29 02:22:17 -0300

    template last change and removal

--- stack.tsx
+++ stack.tsx
@@ -1,4 +1,6 @@
-import { StackServerApp } from '@stackframe/stack';
+'use client';
+
+import { StackApp, StackServerApp } from '@stackframe/stack';
 
 export const stackServerApp = new StackServerApp({
   tokenStore: 'nextjs-cookie',
@@ -8,8 +10,20 @@ export const stackServerApp = new StackServerApp({
   urls: {
     signIn: '/handler/sign-in',
     signUp: '/handler/sign-up',
-    afterSignIn: '/tools',
-    afterSignUp: '/tools',
+    afterSignIn: '/dashboard',
+    afterSignUp: '/onboarding',
+    home: '/',
+  },
+});
+
+export const stackApp = new StackApp({
+  projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID!,
+  publishableClientKey: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY!,
+  urls: {
+    signIn: '/handler/sign-in',
+    signUp: '/handler/sign-up',
+    afterSignIn: '/dashboard',
+    afterSignUp: '/onboarding',
     home: '/',
   },
 });
